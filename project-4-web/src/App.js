3256d35228920dd56bf017337d57531b7ee6c099
Thomas Kerr <kerrthomas06@gmail.com>
2021-12-23 21:49:52 -0500

    Setting up handleSearch and fetchSearch functions.

--- project-4-web/src/App.js
+++ project-4-web/src/App.js
@@ -1,72 +1,18 @@
 import './App.css';
 import { useState, useEffect } from "react";
 
-const Api = (props) => {
-  const [search, setSearch] = useState("");
-  const [stock, setStock] = useState({});
-  const [quantity, setQuantity ] = useState(0);
-};
-
-const handleSearch = (event) => {
-  let newSearch = event.target.value;
-  setSearch(newSearch);
-};
-
-const handleQuantity = (event) => {
-  setQuantity(event.target.value);
-};
-
-const handleBuy = () => {
-  console.log("handleBuy is in effect.");
-  if(sessionStorage.length > 0 && quantity > 0) {
-    let newWallet = parseFloat(props.wallet - (stock.value * quantity)).toFixed(2);
-    let oldPortfolio = props.portfolio.map((item, idx) => {
-      if(item.includes(stock.symbol)) {
-        return true;
-      } else {
-        return false;
-      };
-    });
-    if (newWallet >= 0) {
-      console.log("oldPortfolio: ", oldPortfolio)
-      if (oldPortfolio.includes(true)) {
-        setQuantity(0)
-        return alert("You have this stock in your portfolio. Buy more shares from there.");
-      }
-      else {
-        props.setWallet(newWallet);
-        props.setPortfolio([...props.portfolio, [stock.symbol, quantity, parseFloat(stock.value).toFixed(2)]]);
-        setQuantity(0)
-        setStock({});
-      };
-  };
-};
-};
-
-const fetchSearch = async () => {
-  let symbol = search;
-  console.log(`http://localhost:3000/api/search/${symbol}`)
-  let data = await fetch(`http://localhost:3000/api/search/${symbol}`);
-  data = await data.json();
-  if (data.error) {
-    return alert("Data for this symbol could not be found.");
-  }
-  else {
-    setStockData({ symbol: symbol, value: data.data.price.toFixed(2) });
-    fetchChart(symbol);
-  }
-}
-
-const fetchChart = async (symbol) => {
-  let chart = await fetch(`http://localhost:3000/api/chart?symbol=${symbol}`);
-  chart = await chart.json();
-  document.getElementById("chart-img").setAttribute('src', chart.img);
-  
-}
-
 function App() {
-  const [portfolio, setPortfolio] = useState(JSON.parse(sessionStorage.getItem("collection")) || []);
-  const [wallet, setWallet] = useState(sessionStorage.getItem("wallet") || 5000);
+  const [stock, setStock] = useState[""];
+  const [quantity, setQuantity] = useState[(0)];
+
+  const handleSearch = (event) => {
+    console.log('handleSearch is working.');
+  };
+
+  const fetchSearch = (event) => {
+    console.log('fetchSearch is working.');
+  };
+
   return (
     <>
       <h1>Stock Manager</h1>
@@ -75,8 +21,8 @@ function App() {
         <div className='stock-container'>
           <strong>Stock</strong>
           <h2>Which Stock Would You Like To See?</h2>
-          <input type="text" onChange={handleSearch} placeholder='Search for a stock'/>
-          <button type="submit" onClick={fetchSearch}>Search</button>
+          <input type="text" onChange={handleSearch} placeholder='Search for a stock' />
+          <button className='searchbtn' type="submit" onClick={fetchSearch}>Search</button>
         </div>
         <div className='portfolio-container'>
           <strong>Portfolio</strong>
