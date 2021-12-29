74fe347688a28765f833b4c73d60c8862ca5cb72
Thomas Kerr <kerrthomas06@gmail.com>
2021-12-28 22:35:09 -0500

    Buy button allows stocks to be bought.

--- project-4-web/src/App.js
+++ project-4-web/src/App.js
@@ -10,14 +10,16 @@ function App() {
   const [stock, setStock] = useState("");
   const [results, setResults] = useState({});
   const [quantity, setQuantity] = useState(0);
+  const [portfolio, setPortfolio] = useState([]);
+  const [money, setMoney] = useState(1000);
 
   const handleSearch = (event) => {
-    console.log('handleSearch is working.');
+    console.log("handleSearch is working.");
     setStock(event.target.value);
   };
 
   const fetchSearch = async(event) => {
-    console.log('fetchSearch is working.');
+    console.log("fetchSearch is working.");
     let search = await fetch(`http://localhost:3000/api/yahoo/${stock}`); // Update this route
     search = await search.json(); // Now that you have the json of the response, do something with it
     if(!search.error) {
@@ -25,8 +27,25 @@ function App() {
     }
     else {
       alert('Data not found.');
-    } 
+    }
   }; 
+
+  const buyStock = async(event) => {
+    console.log("The Buy button was clicked.");
+    let stocksBought = parseFloat(results.price * quantity).toFixed(2);
+    if(money >= stocksBought) {
+      setMoney(money - stocksBought);
+      console.log(stock);
+      console.log(quantity);
+      console.log(stocksBought);
+    }
+    else {
+      alert("You do not have enough money to invest in this stock.");
+    }
+  };
+
+  console.log(results.api);
+  console.log(results);
 
   return (
     <>
@@ -35,9 +54,14 @@ function App() {
         <div className='stock-container'>
           <strong>Stock</strong>
           <h2>Which Stock Would You Like To See?</h2>
-          <input type="text" onChange={handleSearch} value={stock} placeholder='Search for a stock' />
+          <input type="text" onChange={handleSearch} value={stock} placeholder="Search for a stock" />
           <button className='searchbtn' type="submit"  onClick={fetchSearch}>Search</button>
-          {(results != "" && results != null) && (<div>{results.price}</div>)}
+          {(results && results.price) && (
+          <>
+          <div>{results.price}</div>
+          <div><button style={{backgroundColor: "green"}} onClick={buyStock}>Buy</button></div>
+          <div><input type="number" onChange={(event) => setQuantity(event.target.value)} value={quantity} style={{width: "250px", marginTop: "10px"}} placeholder="How many stock would you like to buy?" min="1" max="10"/></div>
+          </>)}
         </div>
         <div className='portfolio-container'>
           <strong>Portfolio</strong>
@@ -47,10 +71,11 @@ function App() {
             <div className='grid-header grid-item'>Value</div>
             <div className='grid-header grid-item'>Buy/Sell</div>
           </div>
+
         </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
